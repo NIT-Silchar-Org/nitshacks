01d93a5fab971f8749609e7559ffc2334e10eed8
Kumar Ashish Ranjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2022-09-09 00:09:43 +0530

    Add files via upload

---
+++ components/elements/hamburger_menu.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Image from 'next/image';
+import top_left_svg from "../../public/Vector 14.svg";
+import bottom_left_svg from "../../public/Vector 15.svg";
+import right_svg from "../../public/Vector 11.svg";
+
+const Hamburger_menu = ({showMenu}) => {
+  // document.querySelector(".right_svg").style.bottom=null;
+  // document.querySelector(".right_svg").style.minHeight=null;
+  return (
+    <div className={showMenu ?"hamburger lg:w-[45vw] w-[100vw] min-h-[100vh] flex items-center justify-center fixed top-0 left-0 z-10 border-2 border-[#FF00BF] transition-all":"hamburger lg:w-[45vw] w-[100vw] min-h-[100vh] flex items-center justify-center fixed top-0 left-[-100vw] lg:left-[-45vw] z-10 border-2 border-[#FF00BF] transition-all"} style={{background:"linear-gradient(to bottom,rgba(70, 190, 212, 0.44),rgba(177, 114, 218, 0.74),rgba(208, 60, 171, 0.55))"}}>
+        <div className="hamburger_menu flex flex-col ">
+        <a href="#" className="hamburger_links">About</a>
+        <a href="#" className="hamburger_links">Events</a>
+        <a href="#" className="hamburger_links">Gallery</a>
+        <a href="#" className="hamburger_links">Contact</a>
+        </div>
+        {/* <img src={rec} alt="rec" className="absolute top-0 left-0 z-[-1]"/> */}
+        <div className="image_wrapper absolute top-3 left-2">
+          <Image src={top_left_svg} alt="top_left" className="min-h-[unset] h-[20vh] lg:h-[25vh]"/>
+        </div>
+        <div className="image_wrapper absolute bottom-3 left-2 lg:bottom-3">
+          <Image src={bottom_left_svg} alt="bottom_left" className="min-h-[unset] h-[20vh] lg:h-[25vh]"/>
+        </div>
+        <div className="image_wrapper absolute top-0 right-1">
+          <Image src={right_svg} alt="right" className="right_svg min-h-[unset] bottom-[unset] h-[100vh]"/>
+        </div>
+    </div>
+  )
+}
+
+export default Hamburger_menu;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,45 @@
-export default function Home() {
+import default_logo from "../public/Default.png";
+import variant_logo from "../public/Variant2.png";
+// import rec from "./assets/Rectangle 2.svg";
+import { useState,useEffect } from "react";
+import Ham_icon from "../components/elements/ham_icon";
+import Social_media_icons from "../components/elements/social_media_icons";
+import Hamburger_menu from "../components/elements/hamburger_menu";
+import Image from 'next/image';
+
+// var logo_counter=false;
+function Hs() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  
   return (
-    <div>
-      <div className="text-xl">Hello</div>
+    <div className="hs">
+      <div className="hero_bg min-h-[100vh] w-[100vw] flex bg-slate-500 bg-[url('../public/tokyo.png')]">
+        <Hamburger_menu showMenu={showMenu}/>
+        
+        <div className="hero_logo flex items-center justify-center w-[100vw] min-h-[100vh]">
+          {/* <img src={default_logo} alt="logo" className={showMenu?"lg:translate-x-[20vw] transition-all":"transition-all"} id="logo" loading="lazy"/> */}
+          <div className={showMenu?"lg:translate-x-[20vw] transition-all lg:scale-75 scale-90 ":"transition-all"}>
+            <Image
+              src={default_logo}
+              alt="logo"
+              id="logo"
+            />
+
+          </div>
+        </div>
+
+        <Ham_icon showMenu={showMenu} setShowMenu={setShowMenu} />
+
+        {/* social media icons */}
+        <Social_media_icons/>
+      </div>
+      <div className="sec2 h-[100vh] w-[130vw] bg-slate-400">
+
+      </div>
+
     </div>
   );
 }
+
+export default Hs;
