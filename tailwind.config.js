530876de2b6eaf6c964a98dd3f8a6e53e9bddeee
Nrittik Sarmah <nrittik43@gmail.com>
2022-09-08 20:22:16 +0530

    tailwind font-config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors:{
+        'bgDark':'#120118',
+        'bgPrimary':'#00132D',
+        'fontColor':'#FEFFFF',
+        'neon1':'#4EDFFF',
+        'neon2':'#D03CAB',
+        'offWhite': '#E7E7E7'
+      },
+      fontFamily:{
+        lemonMilk:['lemonMilk'],
+        roadRage:['roadRage'],
+        mokotoGlitch:['mokotoGlitch'],
+        babaPro:['babaPro']
+      },
+      boxShadow: {
+        gradient: ['-3px -3px 8px 4px #D03CAB, 3px 3px 8px 4px #4EDFFF']
+      },
+      backgroundImage:{
+        '404bg':"url('/assests/Error404/404bg.png')"
+      }
+      
+
+    },
   },
   plugins: [],
 };
