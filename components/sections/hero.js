8ad22a6196afc95556bc917d16fa7cbdf3eb7d6c
rewebdev24 <rohitdhakad101@gmail.com>
2022-09-12 23:19:18 +0530

    glitch-2

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -2,102 +2,116 @@ import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Group from '../../public/img/Group-13.png'
 import styles from '../../styles/Hero.module.css'
-
 const Hero = () => {
   const [font,setFont] = useState('glitch1');
-  const [Top,setTop] = useState('1');
-  const [rightNit,setRightNit] = useState('44');
-  const [leftHack,setLeftHack] = useState('28');
-  const [disp1,setDisp1] = useState('');
-  const [disp2,setDisp2] = useState('hidden');
+  const [Top,setTop] = useState('0');
+  const [rightNit,setRightNit] = useState('15');
+  const [leftHack,setLeftHack] = useState('0');
   const [change,setChange] = useState(true);
 
-  // useEffect(()=>{
-  //  const timer1 =  setTimeout(() => {
-  //     setTop('4')
-  //     setLeftHack('60')
-  //     setRightNit('36')
-  //     setFont('glitch3')
-  //   }, 200);
-  //  const timer1_1 =  setTimeout(() => {
-  //     setFont('glitch5')
-  //   }, 250);
-  //  const timer2 =  setTimeout(() => {
-  //   setFont('glitch2')
-  //   }, 280);
-  //  const timer3 =  setTimeout(() => {
-  //   setFont('glitch3')
-  //   setRightNit('44')
-  //   setLeftHack('28')
-  //   setDisp1('hidden')
-  //   setDisp2('')
-  //   }, 350);
-  //  const timer4 =  setTimeout(() => {
-  //   setFont('glitch3')
-  //   }, 400);
-  //  const timer4_1 =  setTimeout(() => {
-  //   setFont('glitch5')
-  //   }, 420);
-  //  const timer5 =  setTimeout(() => {
-  //    setDisp2('hidden')
-  //   }, 450);
-  //   const timer6 =  setTimeout(() => {
-  //     setFont('glitch1')
-  //     }, 450);
+// NITS GLITCH STYLES //
+const stylesNits = {
+  fontFamily:font,
+  color:'#fff',
+}
+const stylesNits2 = {
+  fontFamily:font,
+  color:'#fff',
+  position:'absolute',
+  top:`${Top}%`,
+  left:`${rightNit}%`,
+}
+//NITS GLITCH STYLE END //
 
-  // const timer = setTimeout(() => {
-  //   setTop('1')
-  //   setDisp1('')
-  //   setDisp2('hidden')
-  //   setFont('glitch1')
-  //   setRightNit('44')
-  //   setLeftHack('28')
-  //   setChange(!change)
-  // }, 1000);
-    
-  //   return () => {
-  //     clearTimeout(timer1)
-  //     clearTimeout(timer1_1)
-  //     clearTimeout(timer2)
-  //     clearTimeout(timer3)
-  //     clearTimeout(timer4)
-  //     clearTimeout(timer4_1)
-  //     clearTimeout(timer5)
-  //     clearTimeout(timer6)
-  //     clearTimeout(timer)
-  //   }
-  // },[change])
+// HACKS GLITCH STYLE //
+const stylsHack = {
+  fontFamily:font,
+}
+
+
+const styleHack2 = {
+  fontFamily:font,
+  position:'absolute',
+  bottom:`${Top}%`,
+  left:`${leftHack}%`
+}
+// HACK GLITCH STYLE ENDS //
+
+
+
+// USE EFFECT FOR TIMEOUTS //
+  useEffect(()=>{
+   const timer1 =  setTimeout(() => {
+      setTop('5')
+      setLeftHack('22')
+      setRightNit('22')
+      setFont('glitch2')
+    }, 200);
+   const timer1_1 =  setTimeout(() => {
+      setFont('glitch5')
+    }, 300);
+   const timer2 =  setTimeout(() => {
+    setFont('glitch3')
+    }, 350);
+   const timer3 =  setTimeout(() => {
+    setFont('glitch4')
+    setRightNit('5')
+    setLeftHack('-10')
+    }, 400);
+   const timer4 =  setTimeout(() => {
+    setFont('glitch3')
+    }, 500);
+    const timer6 =  setTimeout(() => {
+      setTop('0');
+      setRightNit('15');
+      setLeftHack('0')
+      setFont('glitch1')
+      }, 550);
+
+  const timer = setTimeout(() => {
+    setTop('0')
+    setFont('glitch1')
+    setRightNit('15')
+    setLeftHack('0')
+    setChange(!change)
+  }, 1000);
+
+    return () => {
+      clearTimeout(timer1)
+      clearTimeout(timer1_1)
+      clearTimeout(timer2)
+      clearTimeout(timer3)
+      clearTimeout(timer4)
+      clearTimeout(timer6)
+      clearTimeout(timer)
+    }
+  },[change])
 
   
   return (
-    <div className=" flex flex-col w-[40rem] absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]">
+    <div className="flex flex-col absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]">
 
       <div className={`m-auto mb-5 ${styles.text_shadow}`}>
-        <p className={`font-${font} md:text-7xl sm:text-5xl text-4xl `}>NITS</p>
-        {/* <p className={`font-${font} ${disp1} absolute top-${Top} right-${rightNit} md:text-7xl sm:text-5xl text-4xl`}>NITS</p> */}
-        {/* <p className={`font-glitch4 ${disp2}  absolute left-28 top-4 md:text-7xl sm:text-5xl text-4xl`}>NITS</p> */}
+        <p style={stylesNits} className={` sm:text-[5rem] text-5xl`}>NITS</p>
+        <p style={stylesNits2} className={` sm:text-[5rem] text-5xl`}>NITS</p>
       </div>
-
-      <div className="m-auto md:w-80 sm:w-60 w-44">
+      <div className="m-auto md:w-70 sm:w-60 w-44">
         <Image src={Group} alt="Group" />
       </div>
       
       <div className={`text-8xl ${styles.text_shadow}`}>
-        <div className={`text-center font-${font} md:text-7xl sm:text-5xl text-4xl`}>
+        <div style={stylsHack} className={`text-center  sm:text-[5rem] text-5xl`}>
           <p>HACKS</p>
           <p>3.0</p>
         </div>
-        {/* <div className={`text-center font-${font} ${disp1} absolute bottom-${Top} left-${leftHack} md:text-7xl sm:text-5xl text-4xl`}>
+        <div style={styleHack2} className={`text-center  sm:text-[5rem] text-5xl`}>
           <p>HACKS</p>
           <p>3.0</p>
-        </div> */}
-        {/* <div className={`text-center font-glitch4 ${disp2} absolute left-8 -bottom-6 md:text-7xl sm:text-5xl text-4xl`}>
-          <p>HACKS</p>
-          <p>3.0</p>
-        </div> */}
+        </div>
       </div>
       
     </div>
+
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Hero from "../components/sections/hero";
-import Hero2 from "../components/sections/hero2";
+// import Hero2 from "../components/sections/hero2";
 import SocialMedia from "../components/sections/socialMedia";
 // import HeroImage from "../public/img/tokyo-city-aesthetic.png";
 // import SmokeMachine from '@bijection/smoke';
@@ -8,8 +8,8 @@ export default function Home() {
 
   return (
     <div className=" sticky text-xl w-full h-screen bg-[url('../public/img/tokyo-city-aesthetic.png')] bg-cover bg-center">
-      {/* <Hero/> */}
-      <Hero2/>
+      <Hero/>
+      {/* <Hero2/> */}
       <SocialMedia/>
     </div>
   );
