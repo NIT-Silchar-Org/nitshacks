c886985f4224d8354e12b49f5d2ebefed34cb8de
rewebdev24 <rohitdhakad101@gmail.com>
2022-10-05 16:00:46 +0530

    Navbar, About

--- components/sections/AboutTecno.js
+++ components/sections/AboutTecno.js
@@ -8,21 +8,15 @@ const AboutTechno = ({showMenu}) => {
           showMenu ? 'opacity-0 duration-500' : ''
         } flex flex-col duration-500 items-center justify-center pb-5 md:p-8`}
       >
-        <div className="font-babaPro py-2 border-b">ABOUT TECNO</div>
+        <div className="font-babaPro py-2 border-b">ABOUT IIC</div>
       </div>
 
       <div
         className={`${
           showMenu ? 'opacity-0 duration-500' : ''
-        } max-w-3xl rounded-3xl text-base md:text-[1.6rem] uppercase p-8 font-medium md:my-4 text-center leading-9`}
+        } max-w-4xl rounded-3xl text-base md:text-[1.6rem] uppercase p-8 font-medium md:my-4 text-center leading-9`}
       >
-        Tecnoesis is the annual techno-managerial event of NIT Silchar,
-        promising all tech geeks the ideal niche of fascinating events,
-        workshops, competitions and interactions worth a lifetime. This mega
-        event has left its mark as of the most prominent techfests across the
-        country. The cauldron of enthusiasm and knowledge attracts various
-        students, presenting the chance to let their minds run wild with ideads,
-        fostering the inventors of the future.
+        Ministry of Education, Govt. of India has established 'MoE's Innovation Cell' to systematically foster the culture of Innovation amongst all Higher Education Institutions. MIC has envisioned encouraging creation of 'Institution's Innovation Council (IICs)' across selected HEIs. National Institute of Technology Silchar has initiated the efforts for implementing the National innovation and start-up policy (NISP) under the aegis of the Ministry of Education's Innovation Cell (MIC). IIC fosters and encourages growth of institute's students' up and coming start-ups and promotes technological growth through competitions.
       </div>
     </div>
   )
